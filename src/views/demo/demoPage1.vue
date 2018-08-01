a3a99d4f980612c91522237d5b265192e3760163
fhc <1518265554@qq.com>
2018-08-01 17:43:46 +0800

    up

---
+++ src/views/demo/demoPage1.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <div class="app-container">
+    <div>
+      <h1>demo fuck1</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A alias dicta est facere fuga labore nemo quis similique sunt totam.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'demoPage1',
+    data() {
+      return {
+      }
+    },
+    created() {
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style scoped>
+</style>
